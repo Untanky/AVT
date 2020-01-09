f5904fb8e5a87565e7c2e4efe80f44f6e85d67cd
Lukas Grimm <mail@lukasgrimm.com>
2020-01-09 14:52:56 +0100

    Implemented Bar Chart

--- src/visualisation/visualizer.js
+++ src/visualisation/visualizer.js
@@ -5,6 +5,8 @@ import { createElement, createStyle } from '../globals/shadowTreeHelper.js';
 import { getInputStyle } from '../globals/inputStyles.js';
 
 const MAXFREQ = 22050
+const FFT_SIZE = 2048;
+const BAR_VIS_BUCKET_COUNT = 50;
 
 const visualisations = [ 'bar', 'line' ];
 
@@ -41,6 +43,8 @@ export default class VisualizerElement extends AudioElement {
 
     createStyle(getStyle(), this.shadow);
     createStyle(getInputStyle(), this.shadow);
+
+    let script = createElement('script', {src: './src/p5.js'}, this.shadow);
 
     const container = createElement('div', {class: 'visualizer-container'}, this.shadow);
 
@@ -131,6 +135,7 @@ class Visualizer {
         this.height = this.canvas.height
         this.canvasCtx = this.canvas.getContext('2d')
         this.analyser = audioCtx.createAnalyser();
+        this.analyser.fftSize = FFT_SIZE;
         this.bufferLength = this.analyser.frequencyBinCount
         this.dataArray = new Uint8Array(this.bufferLength)
         this.visualizers = {bars: this.barVisualizer}
@@ -144,22 +149,27 @@ class Visualizer {
 
     barVisualizer(context) {
 
-        var barWidth = (context.width / context.dataArray.length);
-        var barHeight;
+      var barWidth = ((context.width - 20) / (BAR_VIS_BUCKET_COUNT)) * 2;
+      var barHeight;
 
-        var x = 0; 
-        for (var i = 0; i < context.dataArray.length; i++) {
-            barHeight = context.dataArray[i];
-            if(i === 0)
-                context.canvasCtx.fillStyle = 'rgb(255,0,0)';
-            if(i === 1)
-                context.canvasCtx.fillStyle = 'rgb(0,255,0)';
-            if(i === 2)
-                context.canvasCtx.fillStyle = 'rgb(0,0,255)';
-            context.canvasCtx.fillRect(x, context.height - barHeight, barWidth, barHeight);
+      let buckets = []
 
-            x += barWidth + 1;
-        }
+      for (let i = 0; i < context.dataArray.length; i++) {
+        const element = context.dataArray[i];
+        const bucketIndex = Math.floor(i/context.dataArray.length * BAR_VIS_BUCKET_COUNT);
+        if(buckets[bucketIndex] === undefined || buckets[bucketIndex] === NaN)
+          buckets[bucketIndex] = 0;
+        buckets[bucketIndex] = Math.max(buckets[bucketIndex], element);
+      }
+
+      var x = 10; 
+      for (var i = 0; i < BAR_VIS_BUCKET_COUNT / 2; i++) {
+          barHeight = buckets[i] / 2;
+          context.canvasCtx.fillStyle = 'hsl(0, 75%, 70%)';
+          context.canvasCtx.fillRect(x, context.height - barHeight, barWidth, barHeight);
+
+          x += barWidth + 1;
+      }
     }
 
     /**
