eb8179381eb6bf32d6d5b70b6ff4a4931a82ad34
Felix Schröder <felix.schroeder@online-bb.de>
2020-01-26 13:35:14 +0100

    fixed bug with the stop button not changing the text of the play/pause button

--- src/dj/audioPlayer.js
+++ src/dj/audioPlayer.js
@@ -87,7 +87,7 @@ export default class AudioPlayerElement extends AudioElement {
   onStopClicked() {
     
     this.audioPlayer.stop();
-    this.playButton = "Play"
+    this.playButton.textContent = "Play"
   }
 
   onVolumeChanged(value) {
